94988cdb93f8ebfb54dad9474b7d1e41c8b391f2
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-04-06 21:51:55 -0300

    adding css style to componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Testimonio from './componentes/Testimonio';
 
-function App() {
+ function App() {
   return (
     <div className="App">
+     <div className='contenedor-principal'>
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
       <Testimonio />
+     </div>
     </div>
   );
 }
